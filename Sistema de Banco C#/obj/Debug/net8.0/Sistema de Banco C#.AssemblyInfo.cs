b2f1a6a92e8f839872be4ac60db54008c89b9b72
Pedro Henrique Resende Menezes <pedrohrm050315@hotmail.com>
2024-02-23 17:35:58 -0300

    Commit update 3

--- Sistema de Banco C#/obj/Debug/net8.0/Sistema de Banco C#.AssemblyInfo.cs
+++ Sistema de Banco C#/obj/Debug/net8.0/Sistema de Banco C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema de Banco C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165db6dd6cb5ddbfc28d60a5708e32111a1a989b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08cacc6493ace34d9a27c061a292bacf50786f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema de Banco C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema de Banco C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
